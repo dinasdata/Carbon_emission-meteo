e59c176b2250ebf805a640201158c250c755728a
Dina <andrianambintsoadina@Gmail.com>
2025-06-16 20:02:56 +0300

    maybe the final commit for today

--- parameter_analysis.py
+++ parameter_analysis.py
@@ -1,3 +1,5 @@
+"""You just need to call the get_informations() function with the parameters :temperature or rainfall or emissions and the function relations() with the parameters temperature and rainfall """
+"""for the second parameter of these functions you can add a title of your choice """
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,8 +9,8 @@ from sklearn.linear_model import LinearRegression
 meteo = pd.read_excel("mto.xlsx")
 ghg = pd.read_csv("ghg.csv")
 #explatory data analysis
-"""print(meteo.head())
-print(ghg.head())"""
+print(meteo.head())
+print(ghg.head())
 # we don't need to check NA values because there aren't any unavaible values
 #ghg dataset cleaning
 indexes = np.arange(1993,2023).astype("str")
@@ -20,26 +22,29 @@ meteo["mean_temperature"] = (meteo["Tmax(°C)"]+meteo["Tmin(°C)"])/2
 temperature = meteo["mean_temperature"].groupby(by = meteo["date"].dt.year).mean()
 rainfall = meteo["RR(mm)"].groupby(by = meteo["date"].dt.year).sum()
 #plot size configuration
+plt.grid()
 plt.figure(figsize = (30,25))
 plt.rc("xtick",labelsize = 20)
 plt.rc("ytick",labelsize = 25)
 plt.rc("font",size = 25)
+plt.style.use("ggplot")
 font = {"size":35}
 #getting all the needed information
-def get_information(parameter,title):
+def get_information(parameter,title = None):
     #trendline configuration
     model = LinearRegression()
     model.fit(indexes.astype("int64").reshape((30,1)),parameter)
     trend = model.predict(indexes.astype("int64").reshape((30,1)))
     #plotting the distribution
     plt.subplot(2,1,1)
-    plt.plot(indexes,parameter,lw = 6)
-    plt.grid()
+    plt.plot(indexes,parameter,lw = 6,color = "blue")
+    plt.ylabel(title,**font)
     plt.subplot(2,1,2)
-    plt.scatter(indexes,parameter,lw = 20)
+    plt.scatter(indexes,parameter,lw = 20,color = "blue")
     plt.plot(indexes,trend,lw = 6,color = "red")
-    plt.grid()
     plt.title("Annual {} distribution for the Analamanga region(1993-2022)".format(title),**font)
+    plt.ylabel(title,**font)
+    plt.xlabel("Years",**font)
     #additionnal information
     mean = np.mean(parameter)
     std = np.std(parameter)
@@ -63,14 +68,14 @@ def get_information(parameter,title):
 
 
 #getting the relation between all the parameters
-def relation(y,y_title):
+def relation(y,y_title = None):
     coeff1,p1 = pearsonr(emissions,y)
     if p1 > 0.05:
         print("Absence of relation with carbon dioxyde emission")
         print("Correlation coefficient",coeff1)
     
     else:
-    
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
             print("Presence of relation with carbon dioxyde emission")
             print("Correlation coefficient",coeff1)
             
@@ -78,7 +83,7 @@ def relation(y,y_title):
     plt.grid()
     axis1.set_xlabel("Year",**font)
     axis1.set_ylabel("Carbon dioxyde emission (MtCO2equ)",**font)
-    axis1.plot(indexes,emissions,lw = 6,color = "red",label = "Carbon dioxyde emission[MtCO2equ]")
+    axis1.plot(indexes,emissions,lw = 6,color = "blue",label = "Carbon dioxyde emission[MtCO2equ]")
     plt.legend(loc = "upper left")
     axis2 = axis1.twinx()
     axis2.set_ylabel(y_title,**font)
@@ -86,5 +91,3 @@ def relation(y,y_title):
     plt.legend(loc = "lower right")
     
 
-
-    
