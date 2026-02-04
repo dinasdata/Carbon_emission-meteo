b0a6a46117bde7331675cb06ee412955e8c723b4
dina <andrianambinintsoadina@Gmail.com>
2026-02-04 14:18:29 +0300

    deployment modification

--- training/model_deployment.py
+++ training/model_deployment.py
@@ -1,9 +1,14 @@
 
 from fastapi import FastAPI 
 from pydantic import BaseModel
-import model_prediction 
 import numpy as np
+import joblib
 
+#model importation
+linear_regression = joblib.load("/media/dina/f4c07323-3819-4c76-ad53-95f7d45b7ae2/temperature/models/lr_model.joblib")
+lasso_regression  = joblib.load("/media/dina/f4c07323-3819-4c76-ad53-95f7d45b7ae2/temperature/models/lasso_regression.joblib")
+poly_regression = joblib.load("/media/dina/f4c07323-3819-4c76-ad53-95f7d45b7ae2/temperature/models/poly_regression.joblib")
+svm = joblib.load("/media/dina/f4c07323-3819-4c76-ad53-95f7d45b7ae2/temperature/models/svm_regressor.joblib")
 # defining input data format
 class Features(BaseModel):
     carbon_dioxyde:float
@@ -12,10 +17,10 @@ class Features(BaseModel):
 app = FastAPI()
 @ app.post("/predict")
 def predict(data:Features):
-    predicted = model_prediction.linear_regression.predict([[data.carbon_dioxyde]])[0]
-    predicted2 = model_prediction.lasso_regression.predict([[data.carbon_dioxyde]])[0]
-    predicted3 = model_prediction.poly_regression.predict([[data.carbon_dioxyde]])[0]
-    predicted4 = model_prediction.svm.predict([[data.carbon_dioxyde]])[0]
+    predicted = linear_regression.predict([[data.carbon_dioxyde]])[0]
+    predicted2 = lasso_regression.predict([[data.carbon_dioxyde]])[0]
+    predicted3 = poly_regression.predict([[data.carbon_dioxyde]])[0]
+    predicted4 = svm.predict([[data.carbon_dioxyde]])[0]
     
     return {"Linear regression prediction":predicted.tolist(),"Lasso regression prediction":predicted2.tolist(),
             "Polynomial regression prediction":predicted3.tolist(),
